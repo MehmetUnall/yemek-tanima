c75d29eb2ac30ab9d25f26d083426bc800644826
MehmetUnall <43719230+MehmetUnall@users.noreply.github.com>
2019-06-12 18:38:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -73,8 +73,10 @@ def predict():
     #file.save(os.path.join('./pictures/', image.filename))
     file_name = os.path.realpath(image.filename)
 
-    file_name = "./" + file_name
-
+    file_name = os.path.realpath(__file__) + "./" + file_name
+	
+	return file_name
+	
     graph = load_graph(model_file)
     t = read_tensor_from_image_file(file_name,
                                     input_height=input_height,
